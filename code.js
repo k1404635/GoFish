b7103684e7a62b1d8fb4c53ef109508bce370a40
separase <73293499+separase@users.noreply.github.com>
2022-02-01 07:53:17 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,50 @@
-
+class Deck{
+    constructor(){
+      this.deck = [];
+      this.reset();
+      this.shuffle();
+    }
+  
+    reset(){
+      this.deck = [];
+  
+      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+      const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]} of ${suits[suit]}`);
+        }
+      }
+    }
+  
+    shuffle(){
+      const { deck } = this;
+      let m = deck.length, i;
+  
+      while(m){
+        i = Math.floor(Math.random() * m--);
+  
+        [deck[m], deck[i]] = [deck[i], deck[m]];
+      }
+  
+      return this;
+    }
+  
+    deal(){
+      return this.deck.pop();
+    }
+}
+  
+function gameStart(){
+    var deck1 = new Deck();
+    p1_cards=[];
+    p2_cards=[];
+    for(let i = 0; i < 7; i++){
+        p1_cards[i]=deck1.deal();
+    }
+    for(let i = 0; i < 7; i++){
+        p2_cards[i]=deck1.deal();
+    }
+}  
+  
