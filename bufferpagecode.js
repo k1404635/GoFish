037c038c5bf8d2a2f77a0e85d0a2e15b95a877e6
k1404635 <61251798+k1404635@users.noreply.github.com>
2022-02-03 07:25:32 -0600

    Create bufferpagecode.js

---
+++ bufferpagecode.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('change-player').addEventListener('click',() =>{
+      window.location.href = 'mainpage.html'
+      gameStart();
+    });
+});
